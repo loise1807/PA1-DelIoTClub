06cd05960a96746ebf3e78378f2bd05328d81a17
loise1807 <65726717+loise1807@users.noreply.github.com>
2020-06-11 10:05:23 +0700

    ada sedikit bug

--- admin/header.php
+++ admin/header.php
@@ -10,7 +10,7 @@
   <body>
     <nav class="nav justify-content-end bg-light">
       <div class="col-11 col-sm-11">
-        <a href="index.php">
+        <a href="<?=BASEURL;?>/admin/index.php">
           <img src="<?= BASEURL; ?>/public/img/del.png" style="width:50px;length:50px;">
         </a>
         <h7 style ="color:black;">Institut Teknologi Del</h7>
@@ -27,13 +27,13 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container">
         <img src="<?=BASEURL;?>/public/img/Deliotclub.png" style="width:45px;length:45px;">
-        <a class="navbar-brand" href="index.php">Del IoT Club</a>
+        <a class="navbar-brand" href="<?=BASEURL;?>/admin/index.php">Del IoT Club</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <div class="navbar-nav">
-            <a class="nav-item nav-link justify-content-end active" href="index.php">Home <span class="sr-only">(current)</span></a>
+            <a class="nav-item nav-link justify-content-end active" href="<?=BASEURL;?>/admin/index.php">Home <span class="sr-only">(current)</span></a>
             <a class="nav-item nav-link justify-content-end active" href="<?= BASEURL; ?>/admin/pengumuman.php">Pengumuman <span class="sr-only">(current)</span></a>
             <a class="nav-item nav-link ustify-content-end active" href="<?= BASEURL; ?>/admin/artikel.php" style="">Artikel</a>
             <a class="nav-item nav-link ustify-content-end active" href="<?= BASEURL; ?>/admin/proyek.php" style="">Proyek</a>
